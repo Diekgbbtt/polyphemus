0480acb6ba8bf5082714dd197ead718b10afd197
= <diegogobbetti111@gmail.com>
2026-07-02 18:09:51 +0200

    test: bump neo4j Bolt wait to 180s (avoid cold-start flake)

--- tests/test_neo4j_schema.py
+++ tests/test_neo4j_schema.py
@@ -12,7 +12,7 @@ def _driver():
 
 def test_neo4j_constraints_applied():
     subprocess.run(["docker", "compose", "up", "-d", "neo4j"], check=True)
-    driver = wait_for(_driver, timeout=120)
+    driver = wait_for(_driver, timeout=180)
     with driver.session() as s:
         init_schema(s)
         names = {r["name"] for r in s.run("SHOW CONSTRAINTS YIELD name")}
